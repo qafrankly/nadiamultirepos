d663436b41378aea8d577fc95d81b9503f2d5c29
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-30 16:41:53 +0200

    Update index.js

--- t2/index.js
+++ t2/index.js
@@ -5,8 +5,8 @@ class MultiDemo121 extends Component {
   render(){
 	  
     return (
-      <div className='FranklySecondModulesDemo'>
-	      <h2 className='demo_text'>t2: {this.props.text}</h2>
+      <div className='FranklySecondDemo'>
+	      <h2 className='demoo_text'>t2: {this.props.text}</h2>
       </div>
     );
 	  
