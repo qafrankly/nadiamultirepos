c7c7991f2cd4b1a5d7a76f1e5058ac60e2ed77e0
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-30 16:37:38 +0200

    Update and rename nad2/index.js to t2/index.js

--- t2/index.js
+++ t2/index.js
@@ -1,16 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
-class MultiDemo12 extends Component {
+class MultiDemo121 extends Component {
 	
   render(){
 	  
     return (
       <div className='FranklySecondModulesDemo'>
-	      <h2 className='demo_text'>nad2: {this.props.text}</h2>
+	      <h2 className='demo_text'>t2: {this.props.text}</h2>
       </div>
     );
 	  
   }
 }
 
-export default MultiDemo12;
+export default MultiDemo121;
