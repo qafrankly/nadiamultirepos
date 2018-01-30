35195a356666658a0501522c03bf165db5159da9
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-01-30 16:39:36 +0200

    Update and rename nad4/index.js to t4/index.js

--- t4/index.js
+++ t4/index.js
@@ -1,16 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
-class MultiDemo14 extends Component {
+class MultiDemo141 extends Component {
 	
   render(){
 	  
     return (
       <div className='FranklyFourthModulesDemo'>
-	      <h2 className='demo_text'>nad4: {this.props.text}</h2>
+	      <h2 className='demo_text'>t4: {this.props.text}</h2>
       </div>
     );
 	  
   }
 }
 
-export default MultiDemo14;
+export default MultiDemo141;
